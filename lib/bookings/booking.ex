59d191bd16822a417ad5157a17768bafd7feca16
Matheus Heck <matheusheck@users.noreply.github.com>
2021-10-16 21:10:13 +0200

    factory booking

--- lib/bookings/booking.ex
+++ lib/bookings/booking.ex
@@ -3,7 +3,14 @@ defmodule Flightex.Bookings.Booking do
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
+  def build(complete_date, local_origin, local_destination, user_id, id) do
     # TO DO
+    %__module__ {
+    complete_date: complete_date, 
+    local_origin: local_origin, 
+    local_destination: local_destination, 
+    user_id: user_id, 
+    id
+    }
   end
 end
